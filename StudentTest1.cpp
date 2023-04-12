c85bdfe02a8834f0bbf943448d17816d5d38ba60
dor3518@gmail.com <dor3518@gmail.com>
2023-04-12 22:59:43 +0300

    check

--- StudentTest1.cpp
+++ StudentTest1.cpp
@@ -35,6 +35,7 @@ TEST_CASE("The card scheme at the end of the game")
     Player p2("Bob");
     Game game(p1, p2);
     game.playAll();
+    CHECK_NOTHROW(game.printStats());
     int sum = p1.stacksize() + p1.cardesTaken() + p2.stacksize() + p2.cardesTaken();
     CHECK(sum == 52);
 }
@@ -89,12 +90,11 @@ TEST_CASE("The game ends after at most 26 turns")
     Game game(p1, p2);
     int maxTurns = 26;
     int i = 0;
-    for ( ; i < 26 && p1.stacksize()>0; i++ )
+    for (; i < 26 && p1.stacksize() > 0; i++)
     {
         game.playTurn();
     }
-    CHECK(maxTurns >= i );
+    CHECK(maxTurns >= i);
     CHECK(p1.stacksize() == 0);
     CHECK(p2.stacksize() == 0);
 }
-
